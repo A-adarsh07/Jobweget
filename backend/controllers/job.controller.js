bf3332443d6670def48b70b9caed40d3b5ff6c7f
A-adarsh07 <addy.mishra10@gmail.com>
2024-10-10 21:59:18 +0530

    tested the post method in jobs using postman , fixed the bug

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -32,3 +32,69 @@ export const postJob = async (req, res) => {
         console.log(error);
     }
 }
+// For students 
+export const getAllJobs= async (req,res) => {
+    try {
+        const keyword = req.query.keyword || "";
+        const query ={
+            $or:[
+                {title:{$regex:keyword,$options:"i"}},
+                {description:{$regex:keyword, $options:"i"}}
+            ]
+        };
+        const jobs = await Job.find(query);
+        if(!jobs){
+            return res.status(404).json({
+                message:"Jobs not found",
+                success:false
+            })
+        }
+        return res.status(200).json({
+            jobs,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// For students
+export const getJobById= async (req,res) => {
+    try {
+        const jobId= req.params.id;
+        const job= await Job.findById(jobId);
+        if(!job){
+            return res.status(404).json({
+                message:"Jobs not found",
+                success:false
+            })
+        }
+       return  res.status(200).json({
+            job,success:true
+        });
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Admins creating jobs
+export const  getAdminJobs= async(req,res) => {
+    try {
+        const adminId = req.id;
+        const jobs = await Job.find({created_by:adminId});
+        if(!jobs){
+            return res.status(404).json({
+                message:"Jobs not found",
+                success:false
+            })
+        }
+        return res.status(200).json({
+            jobs,
+            success:true
+        })
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
